6c7a6ad933d584c78c002108957bd1a1599a494b
fatemehjj <joolafatemeh652@gmail.com>
2024-03-09 00:42:46 +0330

    create cart model

---
+++ src/main/java/com/design/wallet/shoppingCart/model/Cart.java
@@ -0,0 +1,30 @@
+package com.design.wallet.shoppingCart.model;
+
+import java.util.List;
+
+public class Cart{
+    List<String> book;
+
+    public Cart(List<String> book, String username) {
+        this.book = book;
+        this.username = username;
+    }
+
+    public List<String> getBook() {
+        return book;
+    }
+
+    public void setBook(List<String> book) {
+        this.book = book;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    String username;
+}
