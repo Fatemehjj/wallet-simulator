cf25e60e452bb73501e6db83cc45d65586c5259a
fatemehjj <joolafatemeh652@gmail.com>
2024-03-09 00:43:32 +0330

    add cart repository

---
+++ src/main/java/com/design/wallet/shoppingCart/repository/CartRepository.java
@@ -0,0 +1,33 @@
+package com.design.wallet.shoppingCart.repository;
+
+import com.design.wallet.shoppingCart.model.Cart;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CartRepository {
+    List<Cart> carts = new ArrayList<>();
+
+    public Cart addOrder(String book,String username) {
+        Cart userCart = findCart(username);
+        userCart.getBook().add(book);
+        return userCart;
+    }
+    public Cart findCart(String username){
+        return (Cart) carts.stream()
+                .filter(cart -> cart.getUsername().equalsIgnoreCase(username))
+                .findAny()
+                .orElse(null);
+    }
+    @PostConstruct
+    private void init(){
+        List<String> books= new ArrayList<>();
+        books.add("The Great Gatsby");
+        books.add("In Search of Lost Time");
+        books.add("The Catcher in the Rye");
+        carts.add(new Cart(books,"saied"));
+    }
+}
