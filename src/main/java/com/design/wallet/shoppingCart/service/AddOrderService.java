75ab1e76a3df62ff8f5ac1cf5039a55a326169c8
fatemehjj <joolafatemeh652@gmail.com>
2024-03-13 01:26:32 +0330

    handle exceptions for adding orders

--- src/main/java/com/design/wallet/shoppingCart/service/AddOrderService.java
+++ src/main/java/com/design/wallet/shoppingCart/service/AddOrderService.java
@@ -1,5 +1,6 @@
 package com.design.wallet.shoppingCart.service;
 
+import com.design.wallet.books.repository.BookRepository;
 import com.design.wallet.shoppingCart.model.Cart;
 import com.design.wallet.shoppingCart.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,17 @@ import org.springframework.stereotype.Service;
 public class AddOrderService {
     @Autowired
     CartRepository repository;
+    @Autowired
+    BookRepository bookRepository;
     public Cart addOrder(String book, String username) {
-        return repository.addOrder(book, username);
+        if (repository.findCart(username)!=null && bookRepository.getBooks().containsKey(book)){
+                return repository.addOrder(book, username); }
+
+        else if (bookRepository.getBooks().containsKey(book)){
+               repository.addCart(username);
+                return repository.addOrder(book, username);
+
+        }
+        else throw new RuntimeException("Couldn't add order'");
     }
 }
